8770a22d31ecc505420f532b5c6af77dba8ea581
ImHunter <dolf@land.ru>
2018-01-23 12:28:10 +0500

    22

--- src/BatchExecuter.groovy
+++ src/BatchExecuter.groovy
@@ -51,7 +51,8 @@ class BatchExecuter {
         try {
 
             // ProcessBuilder pb = new ProcessBuilder("cmd.exe /C start /wait ${batFile.getName()}");
-            ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/C", "START", "/WAIT", "/B", "${batFile.getName()}");
+            // ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/C", "START", "/WAIT", "/B", "${batFile.getName()}");
+            ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/C", "CALL", "${batFile.getName()}");
             // ProcessBuilder pb = new ProcessBuilder("cmd.exe /C START /WAIT /B \"${batFile.getName()}\"");
             // pb.environment().plus(envVariables); 
             
