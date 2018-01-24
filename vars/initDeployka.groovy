ab70fff7844136f53e0d718731e388f96a7316e9
ImHunter <dolf@land.ru>
2018-01-24 11:12:33 +0500

    ww

--- vars/initDeployka.groovy
+++ vars/initDeployka.groovy
@@ -1,7 +1,7 @@
 import Energos.Jenkins.OScript.DeploykaHelper;
 
-def call(){
+def call(def pathToDeployka, def pathToServiceEPF){
 
-    new DeploykaHelper(this);
+    new DeploykaHelper(this, pathToDeployka.toString(), pathToServiceEPF.toString());
 
 }
