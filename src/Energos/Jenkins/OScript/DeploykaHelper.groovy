ed932685a7185b416a8ed7a297b27e731202e2ac
ImHunter <dolf@land.ru>
2018-01-24 11:10:45 +0500

    33

--- src/Energos/Jenkins/OScript/DeploykaHelper.groovy
+++ src/Energos/Jenkins/OScript/DeploykaHelper.groovy
@@ -3,4 +3,13 @@ package Energos.Jenkins.OScript;
 
 class DeploykaHelper extends OScriptHelper {
 
+    String pathToDeployka;
+    String pathToServiceEPF;
+
+    DeploykaHelper(Script script, String pathToDeployka, String pathToServiceEPF){
+        super(script);
+        this.pathToDeployka = pathToDeployka;
+        this.pathToServiceEPF = pathToServiceEPF;
+    }
+
 }
