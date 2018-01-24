9b26c9b3406311f76157607350ddc44bf2014b5d
ImHunter <dolf@land.ru>
2018-01-24 13:04:16 +0500

    ww

--- src/Energos/Jenkins/OScript/DeploykaHelper.groovy
+++ src/Energos/Jenkins/OScript/DeploykaHelper.groovy
@@ -4,12 +4,37 @@ package Energos.Jenkins.OScript;
 class DeploykaHelper extends OScriptHelper {
 
     String pathToDeployka;
-    String pathToServiceEPF;
+    Map<String, String> params = [:];
 
-    DeploykaHelper(Script script, String pathToDeployka, String pathToServiceEPF){
+    private String KEY_DB_SERVER = 'dbServer';
+    private String KEY_DB_DATABASE = 'dbDatabase';
+    private String KEY_DB_USER = 'dbUser';
+    private String KEY_DB_PWD = 'dbPwd';
+    private String KEY_PATH_TO_SERVICE_EPF = 'pathToServiceEpf';
+
+    DeploykaHelper(Script script, String pathToDeployka, String pathToServiceEPF = null){
         super(script);
         this.pathToDeployka = pathToDeployka;
-        this.pathToServiceEPF = pathToServiceEPF;
+        setParam((KEY_PATH_TO_SERVICE_EPF), pathToServiceEPF);
+    }
+
+    def setParam(def paramKey, def paramValue){
+        params.put(paramKey, paramValue);
+        params;
+    }
+
+    def setParam(Map<String, String> newParams){
+
+    }
+
+    def setDb(String dbServer, String dbDatabase, String dbUser = null, String dbPwd = null) {
+        setParam([(KEY_DB_DATABASE):dbDatabase, (KEY_DB_SERVER):dbServer]);
+        if (dbUser!=null) {setParam((KEY_DB_USER), dbUser)};
+        if (dbPwd!=null) {setParam((KEY_DB_PWD), dbPwd)};
+    }
+
+    def setDbAuth(String dbUser, String dbPwd) {
+        setParam([(KEY_DB_USER):dbUser, (KEY_DB_PWD):dbPwd]);
     }
 
 }
