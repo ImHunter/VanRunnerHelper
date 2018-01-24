2da4b01e2a43608c67059c6559fa46f6340c7147
ImHunter <dolf@land.ru>
2018-01-24 09:05:10 +0500

    ввод OScript

---
+++ src/OScriptHelper.groovy
@@ -0,0 +1,61 @@
+
+namespace Energos.Jenkins.OScript;
+
+class OScriptHelper {
+
+    protected Script script;
+
+    Integer resultCode;
+    String resultLog;
+    String outputLogEncoding = 'Cp866';
+
+    OScriptHelper(Script paramScript) {
+        script = paramScript;
+    }
+
+    def echo(def msg){
+        script.echo "${msg}";
+    }
+
+    def execScript(String[] params) {
+
+        def readLog = {InputStream st ->
+            String resLog;
+            resLog = new String(st.getBytes(), outputLogEncoding);
+            resLog;
+        }
+
+        Boolean res;
+        String[] initParams = ["oscript"];
+        String[] fullParams = initParams + params;
+
+        try {
+
+            ProcessBuilder pb = new ProcessBuilder(fullParams);
+
+            Process proc = pb.start();
+            proc.waitFor();
+            resultCode = proc.exitValue();
+            resultLog = readLog(proc.getIn());
+            res = execCode==0;
+
+        } finally {
+            // batFile.delete();
+            // log.delete();
+        }
+
+        res;
+
+    }
+
+    def execScript(List<Object> params) {
+        String[] strParams = new String[params.size()];
+        Integer i = 0;
+        params.each{ elem ->
+            strParams[i] = "${elem}".toString();
+            i++;
+        }
+        execScript(strParams);
+    }
+
+}
