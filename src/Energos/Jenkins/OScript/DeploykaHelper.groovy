0d68182f710c58854a545d4026563e683f20dda0
ImHunter <dolf@land.ru>
2018-01-25 09:33:01 +0500

    ww

--- src/Energos/Jenkins/OScript/DeploykaHelper.groovy
+++ src/Energos/Jenkins/OScript/DeploykaHelper.groovy
@@ -85,7 +85,7 @@ class DeploykaHelper extends OScriptHelper {
 
     private def setLockStatus(LockResourcesEnum res, Boolean locked){
         String op = locked ? "lock" : "unlock";
-        String[] execParams = [pathToDeployka, res.desc, op, "-ras", "${pv(KEY_RAS_SERVER)}", "-rac", "${pv(KEY_RAC_UTIL_PATH)}", 
+        String[] execParams = [pathToDeployka, res, op, "-ras", "${pv(KEY_RAS_SERVER)}", "-rac", "${pv(KEY_RAC_UTIL_PATH)}", 
             "-db", "${pv(KEY_DB_DATABASE)}", "-db-user", "${pv(KEY_DB_USER)}", "-db-pwd", "${pv(KEY_DB_PWD)}"];
         if (res==LockResourcesEnum.lrUserSeanse) {
             execParams = execParams + ["-lockuccode", ucCode];
