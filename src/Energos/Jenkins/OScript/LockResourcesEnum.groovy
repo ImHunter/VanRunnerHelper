52ab25ac758a549e48dfb1d637bc96a61014c460
ImHunter <dolf@land.ru>
2018-01-25 09:26:27 +0500

    ee

--- src/Energos/Jenkins/OScript/LockResourcesEnum.groovy
+++ src/Energos/Jenkins/OScript/LockResourcesEnum.groovy
@@ -1,8 +1,8 @@
 package Energos.Jenkins.OScript;
 
 enum LockResourcesEnum{ 
-    lrUserSeanse("session", 1, "Session"), 
-    lrBackgrowndWork("scheduledjobs", 2, "scheduledjobs") 
+    lrUserSeanse("session"), 
+    lrBackgrowndWork("scheduledjobs") 
     // lrUserSeanse, 
     // lrBackgrowndWork 
 }
