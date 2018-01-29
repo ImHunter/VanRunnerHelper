63ed82e5c35aec64a5a47157dc67c166e951d080
ImHunter <dolf@land.ru>
2018-01-29 14:07:38 +0500

    ww

--- src/Energos/Jenkins/OScript/DeploykaHelper.groovy
+++ src/Energos/Jenkins/OScript/DeploykaHelper.groovy
@@ -273,7 +273,7 @@ class DeploykaHelper extends OScriptHelper {
                 .addPair(ParamsEnum.pePathToServiceEpf)
                 .addPair('-uccode', ucCode)
         );
-        echo "reading log: $resultLog"
+        echo ("reading log: ${resultLog}");
         configInfo.readFromLog(resultLog);
         echo configInfo.version;
 
