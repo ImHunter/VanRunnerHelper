92d3496b15b56ebc802b37551dc514afc79ce0ab
ImHunter <dolf@land.ru>
2018-02-02 17:31:39 +0500

    ss

--- src/Energos/Jenkins/OScript/OScriptHelper.groovy
+++ src/Energos/Jenkins/OScript/OScriptHelper.groovy
@@ -16,7 +16,7 @@ class OScriptHelper {
         this.script = script;
     }
 
-    // @NonCPS
+    @NonCPS
     public void selfTest(){
         echo("Включаем режим тестирования");
         isTestMode = true;
