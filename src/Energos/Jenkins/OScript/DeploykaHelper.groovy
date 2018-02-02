b4b196a50b0e19479c1c6b8fb4bfa4c242128033
ImHunter <dolf@land.ru>
2018-02-02 19:45:48 +0500

    ee

--- src/Energos/Jenkins/OScript/DeploykaHelper.groovy
+++ src/Energos/Jenkins/OScript/DeploykaHelper.groovy
@@ -343,9 +343,9 @@ class DeploykaHelper extends OScriptHelper {
 
         // echo ("executing script");
         retVal = execScript(
-                // new ExecParams(this, DeplCommand.dcRun)
-                new ExecParams(this)
-                .addCommand(DeplCommand.dcRun)
+                new ExecParams(this, DeplCommand.dcRun)
+                // new ExecParams(this)
+                // .addCommand(DeplCommand.dcRun)
                 .addValue(ParamsEnum.peDbConnString)
                 .addPair(ParamsEnum.peDbUser)
                 .addPair(ParamsEnum.peDbPwd)
