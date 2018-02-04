20376700a10abbc9c9d21cf45e14a952ca3174e9
ImHunter <dolf@land.ru>
2018-02-04 11:49:52 +0500

    Добавление документации, мелкие плавки

--- vars/initOScript.groovy
+++ vars/initOScript.groovy
@@ -2,7 +2,7 @@ import Energos.Jenkins.OScript.OScriptHelper;
 
 /**
  * Функция initOScript. Служит для получения экземпляра класса OScriptHelper.
- * @return Объект класса OScriptHelper.
+ * @return Объект класса OScriptHelper
  */
 def call(){
 
