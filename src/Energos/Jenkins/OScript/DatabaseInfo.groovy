492fd100286ffb9b49665b5963e07a3d1227b174
ImHunter <dolf@land.ru>
2018-04-05 18:15:25 +0500

    Правка пред-релиза

--- src/Energos/Jenkins/OScript/DatabaseInfo.groovy
+++ src/Energos/Jenkins/OScript/DatabaseInfo.groovy
@@ -5,6 +5,31 @@ class DatabaseInfo {
     Map<String, String> parameters = [:]
 
     void readInfo(String log){
+        String textParameters = getTextParameters(log)
+        fillMapParameters(textParameters)
+    }
+
+    private String getTextParameters(String log){
+
+        def ArrayList<String> retVal = new ArrayList<>()
+
+        boolean markDetected = false
+
+        Scanner scanner = new Scanner(log)
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine()
+            if (!markDetected) {
+                if (line.toUpperCase().contains('ИНФОРМАЦИЯ - Получен УИД базы'.toUpperCase())) {
+                    markDetected = true
+                    scanner.nextLine()
+                }
+            } else
+                retVal.add(line)
+        }
+        scanner.close()
+    }
+
+    private void fillMapParameters(String[] textParameters){
 
     }
 
