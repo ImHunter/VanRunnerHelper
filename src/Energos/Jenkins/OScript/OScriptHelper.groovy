5f9ee84a92366b40dcf894436469511cf30aa566
ImHunter <imhunter74@gmail.com>
2019-04-17 12:19:05 +0500

    переписал OScriptHelper на использование executor

--- src/Energos/Jenkins/OScript/OScriptHelper.groovy
+++ src/Energos/Jenkins/OScript/OScriptHelper.groovy
@@ -49,6 +49,7 @@ class OScriptHelper {
      */
     protected String launchString
     protected Integer currentExecTimeout = null
+    protected CustomBatchExecuter executer
     // endregion
 
     /**
@@ -57,9 +58,13 @@ class OScriptHelper {
      */
     OScriptHelper(Script script) {
         this.script = script
+        createExecuter(script)
     }
 
-    // @NonCPS
+    private void createExecuter(def script){
+        executer = new JenkinsBatchExecuter(script)
+    }
+
     void selfTest(){
         echo("Включаем режим тестирования")
         isTestMode = true
@@ -145,39 +150,47 @@ class OScriptHelper {
         String[] fullParams = initParams + params
         launchString = fullParams.join(' ')
 
+        def executed = true
+
         if (isTestMode) {
             echo("Вызов execScript в тестовом режиме с параметрами $fullParams")
             resultCode = 0
             resultLog = 'Тестовый лог'
 
         } else {
-            ProcessBuilder pb = new ProcessBuilder(fullParams)
-            Process proc = pb.start()
-            try {
-                proc.waitFor()
-                resultLog = readLog(proc.getIn())
-                resultCode = proc.exitValue()
-            } catch (InterruptedException e) {
-                interrupted = true
-                resultLog = e.getMessage()
-            }
-
-            if (interrupted) {
-                while (proc.isAlive()) {
-                    Thread.sleep(2500)
-                    if (maxExecTime!=null && Calendar.getInstance()>maxExecTime) {
-                        resultCode = null
-                        resultLog = 'Прервано по таймауту\n'.concat(readLog(proc.getIn()))
-                        break
-                    }
-                }
-                resultCode = proc.exitValue()
-                resultLog = readLog(proc.getIn())
-            }
+            executed = executer.execute(launchString)
+            resultLog = executer.execLog
+            resultCode = executed ? 0 : 1
         }
-        setCurrentTimeout(null) // сбрасываем значение тайаута
-        res = resultCode==0
-        res
+//            ProcessBuilder pb = new ProcessBuilder(fullParams)
+//            Process proc = pb.start()
+//            try {
+//                proc.waitFor()
+//                resultLog = readLog(proc.getIn())
+//                resultCode = proc.exitValue()
+//            } catch (InterruptedException e) {
+//                interrupted = true
+//                resultLog = e.getMessage()
+//            }
+//
+//            if (interrupted) {
+//                while (proc.isAlive()) {
+//                    Thread.sleep(2500)
+//                    if (maxExecTime!=null && Calendar.getInstance()>maxExecTime) {
+//                        resultCode = null
+//                        resultLog = 'Прервано по таймауту\n'.concat(readLog(proc.getIn()))
+//                        break
+//                    }
+//                }
+//                resultCode = proc.exitValue()
+//                resultLog = readLog(proc.getIn())
+//            }
+//        }
+//        setCurrentTimeout(null) // сбрасываем значение тайаута
+//        res = resultCode==0
+//        res
+        setCurrentTimeout(null)
+        executed
     }
 
     /**
@@ -228,6 +241,7 @@ class OScriptHelper {
 
     public void setCurrentTimeout(Integer secondsTimeout) {
         currentExecTimeout = secondsTimeout
+        executer.execTimeout = secondsTimeout==null ? 0 : secondsTimeout
     }
 
 }
